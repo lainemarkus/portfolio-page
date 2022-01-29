5bf7204c48c717635823ef8da73361359c66f4d4
markuslaine <markus.laine@aalto.fi>
2022-01-29 17:35:23 +0200

    Created modals for projects

---
+++ src/components/modal.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { getImage, GatsbyImage } from 'gatsby-plugin-image'
+
+
+
+
+const myModal = (props) => {
+
+    if (!props.show) {
+        return null
+    }
+    const data = props.data;
+
+    console.log(data)
+
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <div className="modal-image">
+                    <GatsbyImage image={getImage(data.frontmatter.image)} alt="kuva"/>
+                   
+                </div>
+                <div className="modal-footer">
+                    <h2 className="modal-heading">{data.frontmatter.title}</h2>
+                    <h3 className="modal-subheading">{data.frontmatter.subtitle}</h3>
+                    <p>{data.rawMarkdownBody}</p>
+                    <div className="modal-buttons">
+                        <button className="button" onClick={props.onClose}>Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    )
+
+}
+
+export default myModal
