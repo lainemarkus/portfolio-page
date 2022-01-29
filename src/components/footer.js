a245654bea579d82b104f4b77b975d659a86885d
markuslaine <markus.laine@aalto.fi>
2022-01-29 17:31:39 +0200

    Added Footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p>Created by Markus Laine with GatsbyJS, 2021</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
